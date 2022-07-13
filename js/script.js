fbbfa9bee41151bcc8418490f1a5ae11742e7d65
Carlos H L Santos <64669408+carloshenyk@users.noreply.github.com>
2022-07-13 14:38:34 -0300

    correção de bugs

--- js/script.js
+++ js/script.js
@@ -4,13 +4,6 @@ var ul = document.querySelector('nav ul');
 function menuShow() {
     if (ul.classList.contains('open')) {
         ul.classList.remove('open');
-    }else{
-        ul.classList.add('open');
-    } 
-}
-function menuX(){
-    if (ul.classList.contains('open')) {
-        ul.classList.remove('open');
         document.querySelector('.menu').src = "img/menu_mobile.svg";
     }else{
         ul.classList.add('open');
