bbaf19b84cac0af5146170a32e5a3a9adebf834f
Carlos H L Santos <64669408+carloshenyk@users.noreply.github.com>
2022-07-17 17:29:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ function menuShow() {
         document.querySelector('.menu').src = "img/menu_mobile-X.svg";
     } 
 }
+
+/**********************************************************************
+***********************************************************************
+************************* ANIMAÇÃO DO HEADER **************************
+***********************************************************************
+***********************************************************************/
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
+
+
+for(var i =1; i < 400; i++){
+    banner.innerHTML += "<div class='blocks'></div>";
+        const duration = Math.random() * 5;
+        blocks[i].style.animationDuration = 2 + duration + 's';
+        blocks[i].style.animationDelay = duration + 's';   
+}
+
+const section = document.querySelector('section');
+setTimeout(() => {
+    section.classList.add('active')
+}, 14000);
